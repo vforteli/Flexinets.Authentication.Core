c0624b1ec0065090b89a6ddac203ccede0168e77
Unknown <verner@fortelius.net>
2018-02-16 22:00:33 +0200

    validatemodelstatefilter

---
+++ Controllers/ValidateModelStateFilter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace FlexinetsAuthentication.Core
+{
+    public class ValidateModelStateFilter : Attribute, IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new BadRequestObjectResult(context.ModelState);
+            }
+        }
+    }
+}
